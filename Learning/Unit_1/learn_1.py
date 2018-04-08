228f3b36eb147757943d3c2d4aba12c0aebddbdc
WangDooo <wangbc1993@163.com>
2018-04-08 20:40:47 +0800

    1

--- Learning/Unit_1/learn_1.py
+++ Learning/Unit_1/learn_1.py
@@ -1,4 +1,4 @@
-#================================================================
+        #================================================================
 # 网络爬虫简介
 #----------------------------------------------------------------
 
@@ -16,27 +16,75 @@
 
 #========寻找网站的所有者========================================================
 # 使用WHOIS协议查询域名的注册者
+# pip install python-whois
 #----------------------------------------------------------------
-
-#----------------------------------------------------------------
-
-
-#================================================================
-# 
-#----------------------------------------------------------------
-
+# import whois
+# print(whois.whois('appspot.com'))
+# print(whois.whois('www.buseu.cn'))
 #----------------------------------------------------------------
 
 
-#================================================================
-# 
+#=======下载网页=========================================================
+# 确保发生5xx错误时 重新下载
+# 设置用户代理
+#----------------------------------------------------------------
+import urllib.request
+
+def download(url, num_retries=2):
+	print('Downloading:',url)
+	try:
+		#写入User Agent信息
+		head = {}
+		head['User-Agent'] = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
+ 		#创建Request对象
+		req = urllib.request.Request(url, headers=head)
+    	#传入创建好的Request对象
+		response = urllib.request.urlopen(req)
+		html = response.read().decode('UTF-8')
+	except urllib.request.URLError as e:
+		print('Download error:',e.reason)
+		html = None
+		if num_retries > 0:
+			if hasattr(e,'code') and 500<=e.code<600:
+				# recursively retry 5xx HTTP errors
+				return download(url, num_retries-1)
+	return html
+
+# url = 'http://www.buseu.cn/'
+# # url = 'http://httpstat.us/500'
+# # url = 'http://www.meetup.com/'
+# html = download(url)
+# print(html)
 #----------------------------------------------------------------
 
+
+#========网站地图爬虫========================================================
+# robots.txt 从<loc>标签中提取出URL
+#----------------------------------------------------------------
+# import re
+
+# def crawl_sitemap(url):
+# 	# download the sitemap file	
+# 	sitemap = download(url)
+# 	# extract the sitemap links
+# 	links = re.findall('<loc>(.*?)</loc>',sitemap)
+# 	# download each link
+# 	for link in links:
+# 		html = download(link)
+# 		# scrape html here ...
+
+# crawl_sitemap('http://example.webscraping.com/robots.txt')
 #----------------------------------------------------------------
 
 
-#================================================================
-# 
+#========ID遍历爬虫========================================================
+# http://example.webscraping.com/places/default/view/Afghanistan-1
+# http://example.webscraping.com/places/default/view/Aland-Islands-2
+# http://example.webscraping.com/places/default/view/Albania-3
+# 忽略网站别名 直接遍历ID
+# http://example.webscraping.com/places/default/view/1
+# http://example.webscraping.com/places/default/view/2
+# http://example.webscraping.com/places/default/view/3 
 #----------------------------------------------------------------
 
 #----------------------------------------------------------------
